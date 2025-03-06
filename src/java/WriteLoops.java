6d5a62f4d60401e8770b094e760bfaa259f0471b
isiah-a <isiaha478@gmail.com>
2025-03-06 13:00:02 -0500

    helloZipcode test passed

--- src/java/WriteLoops.java
+++ src/java/WriteLoops.java
@@ -109,16 +109,24 @@ public class WriteLoops {
 
     public int helloZipCode() {
         int w = 0;
-
-        // Write a FOR loop that counts from 5 to 105. Put an IF
-        // statement inside the loop that checks the
-        // loop index counter and if it’s greater than 51,
-        // prints “Hello Zipcode” instead of the statement w = w + 1;
-
-                // calling
+        int i;
+        for (i = 5; i < 105; i++) {
+            if (i > 51) {
+                System.out.println("Hello Zipcode");
+            } else{
                 w = w + 1;
+            }
+
+            // Write a FOR loop that counts from 5 to 105. Put an IF
+            // statement inside the loop that checks the
+            // loop index counter and if it’s greater than 51,
+            // prints “Hello Zipcode” instead of the statement w = w + 1;
+
+            // calling
+
             // each time through the inner loop
-        
+        }
+
         return w;
     }
 
